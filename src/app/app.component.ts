ae86eedf3710d029b163a3d611620bb0f7b36a54
evertonmello <evertonmello@live.com>
2019-04-29 15:06:04 -0300

    calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
+  valueA ='';
+  operation = '';
+  decimal = false
+
+  calculadora = new FormGroup({
+    result: new FormControl(''),
+  });
+
+  inputValue(value){
+    var curValue = this.calculadora.controls.result.value || '';
+    var newValue ;
+
+    if(value == ','){
+      this.decimal = true;
+    }
+    
+    if(this.decimal && value != ','){
+      newValue = curValue.toString() + '.' + value;
+      this.decimal = false
+    }else{
+      newValue = curValue.toString() +  value.toString()
+    }
+
+    this.calculadora.controls.result.setValue(parseFloat(newValue))    
+  }
+
+  setOperation(oprt){
+    this.valueA = this.calculadora.controls.result.value || '';
+    this.calculadora.controls.result.setValue('');
+    this.operation = oprt;
+    this.decimal = false
+  }
+
+  onSubmit() {
+    if(this.calculadora.controls.result.value){
+      var result = eval(this.valueA + this.operation + this.calculadora.controls.result.value || '');  
+      this.calculadora.controls.result.setValue(parseFloat(result))    
+    }
+  }
+
+  clear(){
+    this.decimal = false
+    this.valueA = ''
+    this.calculadora.reset()
+  }
 }
